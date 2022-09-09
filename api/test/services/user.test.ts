4e867337f700267c23c975d551a27b4e0df30632
Freda Manu <101652667+fredamanu@users.noreply.github.com>
2022-09-09 14:13:03 +0200

    adding jest testing for product services

--- api/test/services/user.test.ts
+++ api/test/services/user.test.ts
@@ -1,7 +1,9 @@
 import bcrypt from 'bcrypt'
-import User, { UserDocument } from '../../src/models/User'
+import User from '../../src/models/User'
 import UserServices from '../../src/services/user'
 import connect, { MongodHelper } from '../db-helper'
+
+const nonExistingUserId = '5e57b77b5744fa0b461c7906'
 
 const createUser = async () => {
   const hash = (await bcrypt.hash('gh793jgdjs', 10)) as string
@@ -36,7 +38,7 @@ describe('user service', () => {
     expect(foundUser._id).toEqual(newUser._id)
   })
   it('should find user by email', async () => {
-    const newUser = await createUser()
+    await createUser()
     const user: any = {
       firstName: 'Freda',
       lastName: 'Manu',
@@ -45,6 +47,18 @@ describe('user service', () => {
     }
     const foundUser = await UserServices.findUserByEmail(user)
     expect(foundUser).toHaveProperty('firstName', 'Freda')
+  })
+  it('should not find a non-existing user', async () => {
+    await createUser()
+    const user: any = {
+      firstName: 'Freda',
+      lastName: 'Manu',
+      email: 'freda@gmail.com',
+      password: 'gh793jgdjs',
+    }
+    return await UserServices.findUserByEmail(user).catch((e) => {
+      expect(e.message).toMatch('user not found')
+    })
   })
   it('should find or create user', async () => {
     const newUser = await createUser()
@@ -58,12 +72,20 @@ describe('user service', () => {
     const users = await UserServices.findAllUsers()
     expect(users).toHaveLength(1)
   })
-  it('should update user', async () => {
+  it('should update an existing user', async () => {
     const newUser = await createUser()
     const foundUser = await UserServices.updateUser(newUser._id, {
       firstName: 'Linda',
     })
     expect(foundUser.firstName).toEqual('Linda')
+  })
+  it('should not update a non-exiting user', async () => {
+    await createUser()
+    return await UserServices.updateUser(nonExistingUserId, {
+      firstName: 'Linda',
+    }).catch((e) => {
+      expect(e.message).toMatch(`User ${nonExistingUserId} not found`)
+    })
   })
   it('should delete user', async () => {
     const newUser = await createUser()
